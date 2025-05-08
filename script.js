436d3c81b7c17822be180200d6b5efe9096f9b06
N A G A <rajan555444@gmail.com>
2025-05-08 23:57:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let marks = 50; 
+
+
+if (marks >= 90) {
+  console.log("A Grade")
+} else if (marks >= 80) {
+  console.log("B Grade")
+} else if (marks >= 70) {
+  console.log("C Grade")
+} else if (marks >= 60) {
+  console.log("D Grade")
+} else {
+  console.log("F Grade")
+}
+
